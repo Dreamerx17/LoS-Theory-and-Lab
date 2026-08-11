37cde01def20c66bf7cfc7a2d3406ff3167e7aa0
Dreamerx17 <rkayush2004@gmail.com>
2026-08-11 19:38:00 +0530

    Add LOS Lab Assignment 1

---
+++ LOS_Lab1/check_tool.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+#!/usr/bin/env fish
+
+echo "Enter the name of the tool:"
+read tool
+
+if command -v $tool > /dev/null
+    echo "$tool is installed."
+else
+    echo "$tool is not installed."
+end
---
+++ LOS_Lab1/update_system.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+echo "Updating system..."
+
+sudo pacman -Syu
+
+echo "System update completed."
